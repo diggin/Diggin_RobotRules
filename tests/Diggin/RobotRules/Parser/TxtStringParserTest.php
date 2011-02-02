f944f7a0814a085dbc48ff7425f36d642ef5a2a9
kazusuke sasezaki <sasezaki+github@gmail.com>
2011-02-02 22:54:44 +0900

    add TxtSringParserTest

---
+++ tests/Diggin/RobotRules/Parser/TxtStringParserTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Diggin\RobotRules\Parser;
+use Diggin\RobotRules\Rules\Txt\LineEntity as Line;
+
+class TxtStringParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBlank()
+    {
+$txt = <<<EOF
+
+EOF;
+
+        $txtContainer = TxtStringParser::parse($txt);
+        $this->assertEquals(null, $txtContainer->current(), 'parser should return null, if no field');
+    }
+
+
+    public function testParseLine()
+    {
+        // not robots.txt line
+        $ret = TxtStringParser::parseLine('');
+        $this->assertFalse($ret);
+        $ret = TxtStringParser::parseLine('foo bar');
+        $this->assertFalse($ret);
+        $ret = TxtStringParser::parseLine('foo#bar');
+        $this->assertFalse($ret);
+
+        $ret = TxtStringParser::parseLine('Disallow: /foo/');
+        $this->assertTrue($ret instanceof Line);
+        $this->assertEquals('disallow', $ret->getField());
+        $this->assertEquals('/foo/', $ret->getValue());
+        $this->assertEquals("", $ret->getComment());
+        
+        // handle comment
+        $ret = TxtStringParser::parseLine("Disallow: /foo/ #comment1");
+        $this->assertTrue($ret instanceof Line, var_export($ret,true));
+        $this->assertEquals("comment1", $ret->getComment());
+
+
+        $ret = TxtStringParser::parseLine("User-Agent: Agent1 Agent2 #comment2");
+        $this->assertInternalType('array', $ret);
+        $agent1 = $ret[0];
+        $agent2 = $ret[1];
+        $this->assertEquals('Agent1', $agent1->getValue());
+        $this->assertEquals('comment2', $agent1->getComment());
+        $this->assertEquals('Agent2', $agent2->getValue());
+        $this->assertEquals('comment2', $agent2->getComment());
+    }
+}
+
